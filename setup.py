2c1bc0d721f7d7dff9ac7f62e8b890824ec8726c
Martin Sandström <martin@marteinn.se>
2018-05-19 06:49:52 +0200

    Update pypi troves

--- setup.py
+++ setup.py
@@ -61,9 +61,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4"
-    ],
-    extras_require={
-    },
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ]
 )
